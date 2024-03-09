fe248d6d7a3651206d518b18181f93b6b4b3cae5
umakant3525 <umakant3525@gmail.com>
2024-03-09 12:41:12 +0530

    one joke api done with get

---
+++ 2frontback/backend/app.js
@@ -0,0 +1,30 @@
+//use type : module for this syntax 
+//import express from 'express'
+
+const express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res,next) => {
+res.send("Server is ready ");
+})
+
+const jokes = [
+    { id: 1, title: "A joke", content: "This is joke 1" },
+    { id: 2, title: "Another joke", content: "This is joke 2" },
+    { id: 3, title: "Yet another joke", content: "This is joke 3" },
+    { id: 4, title: "One more joke", content: "This is joke 4" },
+    { id: 5, title: "The last joke", content: "This is joke 5" }
+  ];
+
+app.get("/jokes", (req,res) => {
+    res.json(jokes)
+})
+  
+
+app.listen(PORT,()=>{
+    console.log(`APP listen on ${PORT}`)});
+
