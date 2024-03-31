74d80cded6463c67ba0992d0fa857ae13e266210
umakant3525 <umakant3525@gmail.com>
2024-04-01 01:21:59 +0530

    generate access token by refreshtoken

--- 4Backend/src/controllers/user.controller.js
+++ 4Backend/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudnary.js"; 
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -151,8 +152,57 @@ const logoutUser = asyncHandler( async (req, res) => {
  )
 })
 
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized request");
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken._id);
+    
+        if (!user) {
+            throw new ApiError(401, "Invalid Refresh Token");
+        }
+    
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is invalid or used ");
+        }
+    
+        const options = {
+            httpOnly : true,
+            secure : true
+        }
+      
+        const {newaccessToken, newrefreshToken} = await generateAccessAndRefreshToken(user._id)
+    
+        return res
+        .status(200)
+        .cookie('accessToken', newaccessToken, options)
+        .cookie('accessToken', newrefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken : newaccessToken,
+                   refreshToken : newrefreshToken
+                },
+                "RefreshToken token refreshed successfully "
+            )
+        )
+    } catch (error) {
+        ApiError(401,error?.message || "Invalid request ")
+    }
+
+});
+
+
 export { 
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 };
